34d2e8796da957e6e99b60fa6f06cd3b332cb3ed
Anette Pedersen <anettep85@gmail.com>
2022-08-30 20:04:42 +0200

    sass test

--- holidaze/pages/_app.js
+++ holidaze/pages/_app.js
@@ -1,7 +1,8 @@
 import SSRProvider from "react-bootstrap/SSRProvider";
 import Layout from "../components/layout";
 import "../src/scss/styles.scss";
-import "bootstrap/dist/css/bootstrap.min.css";
+
+// import "bootstrap/dist/css/bootstrap.min.css";
 
 function MyApp({ Component, pageProps }) {
   return (
