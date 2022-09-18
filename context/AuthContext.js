92bc53eda38d2545ef34e0d03ab4bbe8e20caa08
Anette Pedersen <anettep85@gmail.com>
2022-09-18 15:57:04 +0200

    login skjema

--- context/AuthContext.js
+++ context/AuthContext.js
@@ -1,16 +1,32 @@
-import { createContext, useState } from "react";
+// import { createContext, useState } from "react";
 
-const AuthContext = createContext({
-  user: null,
-  login: () => {},
-  logout: () => {},
-  authReady: false,
-});
+// const AuthContext = createContext({
+//   user: null,
+//   login: () => {},
+//   logout: () => {},
+//   authReady: false,
+// });
+
+// export const AuthProvider = (props) => {
+//   const [user, setUser] = useState(null);
+//   return (
+//     <AuthContext.Provider value={[user, setUser]}>
+//       {props.children}
+//     </AuthContext.Provider>
+//   );
+// };
+
+// export default AuthContext;
+
+import React, { useState } from "react";
+import useLocalStorage from "../hooks/useLocalStorage";
+
+const AuthContext = React.createContext([null, () => {}]);
 
 export const AuthProvider = (props) => {
-  const [user, setUser] = useState(null);
+  const [auth, setAuth] = useLocalStorage("auth");
   return (
-    <AuthContext.Provider value={[user, setUser]}>
+    <AuthContext.Provider value={[auth, setAuth]}>
       {props.children}
     </AuthContext.Provider>
   );
