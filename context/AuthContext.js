05dc6a02bde1610741e90740d42e9ce7940c5ba1
Anette Pedersen <anettep85@gmail.com>
2022-09-18 17:15:57 +0200

    login form

--- context/AuthContext.js
+++ context/AuthContext.js
@@ -1,32 +1,16 @@
-// import { createContext, useState } from "react";
+import { createContext, useState } from "react";
 
-// const AuthContext = createContext({
-//   user: null,
-//   login: () => {},
-//   logout: () => {},
-//   authReady: false,
-// });
-
-// export const AuthProvider = (props) => {
-//   const [user, setUser] = useState(null);
-//   return (
-//     <AuthContext.Provider value={[user, setUser]}>
-//       {props.children}
-//     </AuthContext.Provider>
-//   );
-// };
-
-// export default AuthContext;
-
-import React, { useState } from "react";
-import useLocalStorage from "../hooks/useLocalStorage";
-
-const AuthContext = React.createContext([null, () => {}]);
+const AuthContext = createContext({
+  user: null,
+  login: () => {},
+  logout: () => {},
+  authReady: false,
+});
 
 export const AuthProvider = (props) => {
-  const [auth, setAuth] = useLocalStorage("auth");
+  const [user, setUser] = useState(null);
   return (
-    <AuthContext.Provider value={[auth, setAuth]}>
+    <AuthContext.Provider value={[user, setUser]}>
       {props.children}
     </AuthContext.Provider>
   );
