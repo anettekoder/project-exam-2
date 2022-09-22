e6923472a09fdcda225edb462fb785cd141ffdf5
Anette Pedersen <anettep85@gmail.com>
2022-09-22 21:23:52 +0200

    detail id

--- components/admin/AdminEnquiries.js
+++ components/admin/AdminEnquiries.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Moment from "react-moment";
+import { Table } from "react-bootstrap";
 
 function AdminEnquiries({ enquiries }) {
   const [selected, setSelected] = useState(null);
@@ -18,71 +19,78 @@ function AdminEnquiries({ enquiries }) {
         <h2 className="text-2xl md:text-3xl xl:text-4xl xl:pb-2">Enquiries</h2>
       </div>
       <div>
-        <div className="flex md:table w-full">
-          <div className="hidden md:table-header-group font-heading">
-            <div className="table-row text-left">
-              <div className="table-cell pl-2 w-1/4 font-normal">Hotel</div>
-              <div className="table-cell border-l border-gray-300 pl-2 w-1/4 font-normal">
-                From
-              </div>
-              <div className="table-cell border-l border-gray-300 pl-2 w-1/4 font-normal">
-                To
-              </div>
-              <div className="table-cell border-l border-gray-300 pl-2 w-1/2 font-normal">
-                Name
-              </div>
-              <div className="table-cell border-l border-gray-300 pl-2 w-1/2 font-normal">
-                Email
+        <Table striped bordered hover>
+          <div className="flex md:table w-full">
+            <div className="hidden md:table-header-group font-heading">
+              <div className="table-row text-left">
+                <div className="table-cell pl-2 w-1/4 font-normal">Hotel</div>
+
+                <div className="table-cell border-l border-gray-300 pl-2 w-1/4 font-normal">
+                  From
+                </div>
+                <div className="table-cell border-l border-gray-300 pl-2 w-1/4 font-normal">
+                  To
+                </div>
+                <div className="table-cell border-l border-gray-300 pl-2 w-1/2 font-normal">
+                  Name
+                </div>
+                <div className="table-cell border-l border-gray-300 pl-2 w-1/2 font-normal">
+                  Email
+                </div>
               </div>
             </div>
-          </div>
-          <div className="text-white flex flex-col w-full md:table-row-group font-paragraph">
-            {enquiries &&
-              enquiries.data?.map((enquiry, i) => {
-                return (
-                  <div
-                    key={enquiry.id}
-                    onClick={() => toggle(i)}
-                    className="flex flex-wrap justify-between items-center md:table-row bg-blue-600 odd:bg-blue-500 lg:hover:bg-blue-400"
-                  >
-                    <div className="md:table-cell px-2 py-4 w-1/3 sm:w-1/2 md:w-1/4">
-                      {enquiry.hotel}
-                    </div>
-
-                    <Moment
-                      format="DD/MM/YYYY"
-                      className="md:table-cell text-center sm:text-left px-2 py-4 w-1/3 sm:w-1/4"
-                    >
-                      {enquiry.from}
-                    </Moment>
-                    <Moment
-                      format="DD/MM/YYYY"
-                      className="md:table-cell px-2 py-4 w-1/3 sm:w-1/4"
-                    >
-                      {enquiry.to}
-                    </Moment>
-                    <div className="px-2 py-4 w-20 md:w-1/2 hidden md:table-cell">
-                      {enquiry.name}
-                    </div>
-                    <div className="px-2 py-4 md:w-1/2 hidden md:table-cell">
-                      {enquiry.email}
-                    </div>
+            <div className="text-black flex flex-col w-full md:table-row-group font-paragraph">
+              {enquiries &&
+                enquiries.data?.map((enquiry, i) => {
+                  return (
                     <div
-                      className={
-                        selected == i
-                          ? "w-full block md:hidden"
-                          : "w-full hidden md:hidden"
-                      }
+                      key={enquiry.attributes.id}
+                      onClick={() => toggle(i)}
+                      className="flex flex-wrap justify-between items-center md:table-row bg-blue-600 odd:bg-blue-500 lg:hover:bg-blue-400"
                     >
-                      <div className="w-11/12 h-0.5 bg-white mx-auto"></div>
-                      <div className="p-2 w-1/2">{enquiry.name}</div>
-                      <div className="p-2 w-1/4">{enquiry.email}</div>
+                      <div className="md:table-cell px-2 py-4 w-1/3 sm:w-1/2 md:w-1/4">
+                        {enquiry.attributes.hotel}
+                      </div>
+
+                      <Moment
+                        format="DD/MM/YYYY"
+                        className="md:table-cell text-center sm:text-left px-2 py-4 w-1/3 sm:w-1/4"
+                      >
+                        {enquiry.attributes.from}
+                      </Moment>
+                      <Moment
+                        format="DD/MM/YYYY"
+                        className="md:table-cell px-2 py-4 w-1/3 sm:w-1/4"
+                      >
+                        {enquiry.to}
+                      </Moment>
+                      <div className="px-2 py-4 w-20 md:w-1/2 hidden md:table-cell">
+                        {enquiry.attributes.name}
+                      </div>
+                      <div className="px-2 py-4 md:w-1/2 hidden md:table-cell">
+                        {enquiry.attributes.email}
+                      </div>
+                      <div
+                        className={
+                          selected == i
+                            ? "w-full block md:hidden"
+                            : "w-full hidden md:hidden"
+                        }
+                      >
+                        <div className="w-11/12 h-0.5 bg-white mx-auto"></div>
+                        <div className="p-2 w-1/2">
+                          {enquiry.attributes.name}
+                        </div>
+                        <div className="p-2 w-1/4">
+                          {enquiry.attributes.email}
+                        </div>
+                      </div>
                     </div>
-                  </div>
-                );
-              })}
+                  );
+                })}
+            </div>
           </div>
-        </div>
+        </Table>
       </div>
     </>
   );
--- pages/detail/[id].js
+++ pages/detail/[id].js
@@ -3,7 +3,37 @@ import axios from "axios";
 import Heading from "../../components/heading";
 import { BASE_URL } from "../../constant/api";
 
-const url = BASE_URL + "accomodations";
+export async function getStaticPaths() {
+  try {
+    const response = await axios.get(BASE_URL + "accomodations/?populate=*");
+    console.log(response.data);
+    const hotels = response.data;
+    const paths = hotels.data.map((hotel) => ({
+      params: { id: `${hotel.id}` },
+    }));
+
+    return { paths, fallback: false };
+  } catch (error) {
+    console.log(error);
+  }
+}
+export async function getStaticProps({ params }) {
+  const url = `${BASE_URL}enquiries/${params.id}`;
+
+  let hotel = null;
+
+  try {
+    const response = await axios.get(url);
+
+    hotel = response.data.data;
+  } catch (error) {
+    console.log(error);
+  }
+
+  return {
+    props: { hotel: hotel },
+  };
+}
 
 export default function Details({ hotel }) {
   return (
@@ -16,40 +46,7 @@ export default function Details({ hotel }) {
         headingText="Detail page"
         subHeadingText="Experience Bergen's best hotels"
       />
-      <h1>{hotel.attributes.name}</h1>
+      <h1>{hotel?.attributes.name}</h1>
     </div>
   );
-}
-
-export async function getStaticPaths() {
-  try {
-    const response = await fetch(url);
-
-    const hotels = response.data.data;
-
-    const paths = hotels.map((hotel) => ({
-      params: { id: hotel.id.toString() },
-    }));
-
-    return { paths, fallback: false };
-  } catch (error) {
-    console.log(error);
-  }
-}
-export async function getStaticProps({ params }) {
-  const newUrl = `${BASE_URL}/${params.id}`;
-
-  let hotel = null;
-
-  try {
-    const response = await fetch(newUrl);
-
-    hotel = response.data;
-  } catch (error) {
-    console.log(error);
-  }
-
-  return {
-    props: { hotel: hotel },
-  };
 }
